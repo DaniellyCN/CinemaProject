bb06070ef681e590863ed387da1161d73706e8fe
Danielly <daniellyccn@gmail.com>
2021-12-02 17:37:30 -0300

    Início 3

---
+++ reactjs/src/pages/layoutLugares/index.js
@@ -0,0 +1,8 @@
+export default function LayoutLugares(){
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
--- reactjs/src/routes.js
+++ reactjs/src/routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './pages/home';
 
+import Home from './pages/home';
+import Lugares from './pages/layoutLugares';//tela da disposição das poltronas vagas/ocupadas
 
 
 export default function Routes(){
@@ -8,7 +9,8 @@ export default function Routes(){
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact={true} component={Home} />  
+                <Route path="/" exact={true} component={Home} /> 
+                <Route path="/layout-lugares" component={Lugares} /> 
             </Switch>
         </BrowserRouter>
     )
