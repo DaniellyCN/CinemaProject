f26b8591c68fbfdf1d5b6e5c87c62994f0708771
Danielly <daniellyccn@gmail.com>
2021-12-03 00:25:56 -0300

    Components

--- reactjs/src/components/poltrona/styled.js
+++ reactjs/src/components/poltrona/styled.js
@@ -1,1 +1,14 @@
 import styled from 'styled-components';
+
+const Cadeira = styled.div`
+width: 1em;
+height: 1em;
+position: relative;
+.numero{
+    font: 0.9em Spartan;
+    color: black;
+    position: absolute;
+    
+}
+`
+export {Cadeira}
