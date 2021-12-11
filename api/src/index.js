126d3ca9969f6980d022cb889fb0c4c68daff1be
Joao <joaopedrosilvadossantos2003@gmail.com>
2021-12-11 16:45:48 -0300

    -_-

--- api/src/index.js
+++ api/src/index.js
@@ -9,7 +9,65 @@ app.use(express.json());
 
 
 const conn = await MongoClient.connect('mongodb+srv://devmonk:d3v@cluster.ru31h.mongodb.net')
-const db = await conn.db('livraria').collection('livros')
+const dbSessoes  = await conn.db('ingressos').collection('sessoes')
+const dbLugares  = await conn.db('ingressos').collection('lugares')
+
+
+app.get('/availableDays', async (req, resp) => {
+    try {
+        let news = new Date().toISOString().slice(0, 10);
+        let days = await 
+        dbSessoes
+        .aggregate([ 
+            {
+                $match: {
+                    data: {
+                        $gte: news
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: '$data' //Agrupa todos os documentos//
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    data: '$_id'
+                }
+            },
+            {
+                $limit: 7
+            },
+            {
+                $sort: {
+                    data: 1
+                }
+            }
+        ])
+        .toArray();
+
+
+        resp.send(days);
+
+    } catch (e) {
+        resp.send(e.toString())
+    }
+})
+
+
+app.get('/availableMovies/:date', async (req, resp) => {
+    let { date } = req.params;
+
+    let movies = await
+        dbSessoes
+            .find({ data: date })
+            .project({ _id: 0, horarios: 0 })
+            .toArray();
+    
+    resp.send(movies);
+})
 
 
 app.get('/ping', async (req, resp) => {
